c339e3b977fbbf246d933c3737dde7b0f375cd09
kawax <kawaxbiz@gmail.com>
2022-12-06 21:36:02 +0900

    wip

---
+++ app/Listeners/InteractionsListener.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\InteractionsWebhook;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Http;
+
+class InteractionsListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  InteractionsWebhook  $event
+     * @return void
+     */
+    public function handle(InteractionsWebhook $event)
+    {
+        //
+        $app_id = config('services.discord.bot');
+        $token = $event->request->json('token');
+
+        $data = [
+            'content' => 'Hello! @'.$event->request->json('member.user.id'),
+        ];
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bot '.config('services.discord.token'),
+        ])->post("https://discord.com/api/v10/webhooks/$app_id/$token", $data);
+
+        info($response->json());
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Revolution\DiscordManager\Contracts\InteractionsEvent;
+use Revolution\DiscordManager\Contracts\InteractionsResponse;
+use App\Events\InteractionsWebhook;
+use Revolution\DiscordManager\Http\Response\ChannelMessageResponse;
+use Revolution\DiscordManager\Http\Response\DeferredResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+
+        $this->app->singleton(InteractionsResponse::class, DeferredResponse::class);
+        $this->app->singleton(InteractionsEvent::class, InteractionsWebhook::class);
     }
 
     /**
